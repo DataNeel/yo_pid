3386b8bfbcb16a87542f7528a27980910d57945a
nshivdasani <nshivdasani@god01.(none)>
2014-09-30 15:49:43 +0000

    New code and command option

--- yo_pid.sh
+++ yo_pid.sh
@@ -1,21 +1,49 @@
-#!/bin/bash                                                                                                                                    
+#!/bin/bash                                                                                                                      
 
-if [ $# -ne 2 ];
-then
-   echo "usage: ./yo_pid.sh pid yo_name"
-   exit
-fi
-
-prev_time="0"
-curr_time="$(ps -p $1 -o time,pmem,sgi_p | sed -n 2p)"
-
-
-while [ "$curr_time" != "$prev_time" ]
-do
-    sleep 30
-    a="$curr_time"
-    curr_time="$(ps -p $1 -o time,pmem,sgi_p | sed -n 2p)"
-    prev_time="$a"
+while getopts ":c:u:p:" opt; do
+  case $opt in
+    c) COMMAND="$OPTARG";;
+    u) YONAME=$OPTARG;;
+    p) PID=$OPTARG;;
+    :) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    \?) echo "Invalid option: -$OPTARG" >&2;
+	echo "Use -c to provide a command or -p to provide a PID";
+	echo "Use of -u to specify a Yo name is required";
+	exit 1;;
+  esac
 done
 
-curl --data "api_token={{api key}}&username=$2" http://api.justyo.co/yo/
+if ([ -z "$COMMAND" ] && [ -z $PID ]) || [ -z $YONAME ]
+then
+    echo "Not enough options provided"
+    echo "Use -c to provide a command or -p to provide a PID"
+    echo "Use of -u to specify a Yo name is required"
+    exit 1
+fi
+
+if [ ! -z "$COMMAND" ] && [ ! -z $PID ]
+then
+    echo "Command and PID both provided"
+    echo "Provide only a command using -c or a PID using -p"
+    exit 1
+fi
+
+if [ ! -z "$COMMAND" ]
+then 
+    eval "$COMMAND"
+fi
+
+if [ ! -z $PID ]
+then
+      prev_time="0"
+      curr_time="$(ps -p $PID -o time,pmem,sgi_p | sed -n 2p)"
+      while [ "$curr_time" != "$prev_time" ]
+      do
+	  sleep 30
+	  a="$curr_time"
+	  curr_time="$(ps -p $PID -o time,pmem,sgi_p | sed -n 2p)"
+	  prev_time="$a"
+      done
+fi
+
+curl --data "api_token={{api key}}&username=$YONAME" http://api.justyo.co/yo/
